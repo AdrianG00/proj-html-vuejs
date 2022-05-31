a622669e0203ebfe48a6135b7933318b6f60c5ce
AdrianG00 <giurgicatalin@gmail.com>
2022-05-31 10:20:43 +0200

    Add: header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    
+    <SiteHeader/>
+
+    <SiteMain/>
+
+    <SiteFooter/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import SiteHeader from './components/HeaderComponent.vue'
+import SiteMain from './components/MainComponent.vue'
+import SiteFooter from './components/FooterComponent.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
-  },
-};
+    SiteHeader,
+    SiteMain,
+    SiteFooter
+  }
+}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  @import '@/assets/scss/style.scss';
+
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,63 @@
+<template>
+  
+    <header>
+
+        <div class="container">
+            <div class="row py-3">
+
+                <div class="col d-flex align-items-center">
+
+                    <font-awesome-icon icon="fa-solid fa-clock" />
+                    <span class="mx-2">Open Hours: Mon - Sat - 9:00 - 18:00</span>
+
+                </div>
+
+
+                <div class="col d-flex justify-content-end align-items-center">
+
+                    <div>
+
+                        <font-awesome-icon icon="fa-solid fa-phone" />
+                        <span class="mx-2">+1 (305) 1234-5678</span>
+
+                    </div>
+
+                    <div class="mx-4">
+
+                        <font-awesome-icon icon="fa-solid fa-envelope" />
+                        <span class="mx-2">hello@example.com</span>
+
+                    </div>
+
+                    <div>
+
+                        <font-awesome-icon icon="fa-brands fa-facebook-f" />
+                        <font-awesome-icon icon="fa-brands fa-twitter" class="mx-4" />
+                        <font-awesome-icon icon="fa-brands fa-linkedin-in" />
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+    </header>
+
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+
+    header {
+        background-color: #21333E;
+        color: lightgrey;
+    }
+
+
+</style>
